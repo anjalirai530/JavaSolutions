84b7a33f1277b4d14340f52d949004a1bf497532
anjalirai530 <raianjali1201@gmail.com>
2020-09-11 15:23:13 +0530

    Create LastIndexOfNumberUsingRecursion .java

---
+++ LastIndexOfNumberUsingRecursion .java
@@ -0,0 +1,28 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class LastIndex {
+     
+	public static int findIndex(int A[],int firstIndex,int x)
+	{
+		if(firstIndex < 0)
+			return -1;
+		if(A[firstIndex] == x)
+			return firstIndex;
+		else
+			return findIndex(A,firstIndex-1,x);
+	}
+	
+	public static void main(String[] args) {
+		Scanner s  = new Scanner(System.in);
+		int n = s.nextInt();
+		int A[] = new int[n];
+		for(int i = 0;i<n;i++)
+			A[i] = s.nextInt();
+		int x = s.nextInt();
+		System.out.println(findIndex(A,0,x));
+
+	}
+
+}
