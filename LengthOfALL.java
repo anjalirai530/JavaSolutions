b6a70412f3c6fd512776752eae7c4e3b8fe87301
anjalirai530 <raianjali1201@gmail.com>
2020-09-18 21:35:41 +0530

    Create LengthOfALL.java

---
+++ LengthOfALL.java
@@ -0,0 +1,14 @@
+public class Solution {
+
+	public static int length(LinkedListNode<Integer> head){
+	
+     LinkedListNode<Integer> temp = head;
+        int count = 0;
+        while(temp != null)
+        {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+	}
+}
