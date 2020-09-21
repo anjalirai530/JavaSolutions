19c164b2cdadfa927751fed129cf41bfe036f425
anjalirai530 <raianjali1201@gmail.com>
2020-09-21 18:33:55 +0530

    Create RecursiveInsertInLL.java

---
+++ RecursiveInsertInLL.java
@@ -0,0 +1,26 @@
+public static NodeR<Integer> insertR(NodeR<Integer>head,int val,int index)
+	{
+		
+		if(index == 0)
+		{
+			NodeR<Integer>temp = new NodeR<Integer>(val);
+			temp.next = head;
+	
+			return temp;
+			
+		}
+		else {
+			head.next= insertR(head.next,val,index -1);
+	
+			return head; 
+		}
+		
+	}
+	public static void main(String[] args) {
+		
+		
+       NodeR <Integer>head= insertR(head,90,2);
+        RecursivePrint(head);
+	}
+
+}
